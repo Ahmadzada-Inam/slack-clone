9c9d0160f9b0c6d87f498f5e2ffe36d8dd0c5d81
Ahmadzada-Inam <inam.ahmadzada@outlook.com>
2018-12-15 18:00:29 -0500

    some polishing in login screen

--- app/src/main/java/com/example/ahmadzada/slackclone/Activities/LoginActivity.kt
+++ app/src/main/java/com/example/ahmadzada/slackclone/Activities/LoginActivity.kt
@@ -1,14 +1,17 @@
 package com.example.ahmadzada.slackclone.Activities
 
+import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.content.LocalBroadcastManager
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import com.example.ahmadzada.slackclone.R
 import com.example.ahmadzada.slackclone.Services.AuthService
 import com.example.ahmadzada.slackclone.Utilities.BROADCAST_USER_CHANGE_INTENT
+import kotlinx.android.synthetic.main.activity_create_user.*
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -16,24 +19,35 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        this.loginSpinner.visibility = View.INVISIBLE
     }
 
     fun loginLoginBtnClick(v: View) {
         val userEmail = this.loginEmailTextField.text.toString()
         val userPassword = this.loginPasswordTextField.text.toString()
+        this.enableSpinner(true)
 
+        this.hideKeyboard()
         if (userEmail.isNotEmpty() && userPassword.isNotEmpty()) {
             AuthService.loginUser(this, userEmail, userPassword) { loginSuccess ->
                 if (loginSuccess) {
                     AuthService.findUserByEmail(this) { findUserSuccess ->
-                        val userChangedIntent = Intent(BROADCAST_USER_CHANGE_INTENT)
-                        LocalBroadcastManager.getInstance(this).sendBroadcast(userChangedIntent)
-                        finish()
+                        if (findUserSuccess) {
+                            val userChangedIntent = Intent(BROADCAST_USER_CHANGE_INTENT)
+                            LocalBroadcastManager.getInstance(this).sendBroadcast(userChangedIntent)
+                            this.enableSpinner(false)
+                            finish()
+                        } else {
+                            this.errorToast("Oops, Something went wrong, Pleae try again.")
+                        }
                     }
+                } else {
+                    this.errorToast("Oops, Something went wrong, Pleae try again.")
                 }
             }
         } else {
-            Toast.makeText(this, "Please fill in all the fields.", Toast.LENGTH_LONG).show()
+           this.errorToast("Please fill in all the fields.")
         }
     }
 
@@ -41,5 +55,25 @@ class LoginActivity : AppCompatActivity() {
         val createUserIntent = Intent(this, CreateUserActivity::class.java)
         startActivity(createUserIntent)
         finish()
+    }
+
+    fun errorToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+        this.enableSpinner(false)
+    }
+
+    fun enableSpinner(enable: Boolean) {
+        this.loginSpinner.visibility = if (enable) View.VISIBLE else View.INVISIBLE
+
+        this.loginLoginBtn.isEnabled = !enable
+        this.loginSignupBtn.isEnabled = !enable
+    }
+
+    fun hideKeyboard() {
+        val inputManager = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+        if (inputManager.isAcceptingText) {
+            inputManager.hideSoftInputFromWindow(this.currentFocus.windowToken, 0)
+        }
     }
 }
